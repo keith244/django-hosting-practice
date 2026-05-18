9faaf77852cdac6588f464c4f3e12158f50eab1d
Keith <keithkipngeno24@gmail.com>
2026-05-18 21:23:02 +0300

    Added white noise storage settings

--- host_with_dB/core/settings.py
+++ host_with_dB/core/settings.py
@@ -132,3 +132,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
