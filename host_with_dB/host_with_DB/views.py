88602b9108fef132e9e113cebf86dff06d6e7e33
Keith <keithkipngeno24@gmail.com>
2026-05-25 17:08:29 +0300

    Added a create function for users to interact with the app

--- host_with_dB/host_with_DB/views.py
+++ host_with_dB/host_with_DB/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render,get_object_or_404, redirect
+from django.http import HttpResponse
+from django.core import cache
 from .models import Post
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
     return render(request, 'host_with_DB/index.html',{'posts': posts})
+
+def create_post(request):
+    ip = request.META.get('REMOTE_ADDR')
+
+    # rate limit key
+    cache_key = f"create_post_{ip}"
+
+    #check if user posted recently
+    if cache.get(cache_key):
+        return HttpResponse("Too many requests. Please wiat before creating another post.", status=429)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+        # set rate limit for 1 minute
+        cache.set(cache_key, True, timeout=60)
+        return redirect('index')
+    return render(request, 'host_with_DB/create_post.html') 
 
 def post_read(request,post_id):
     post = get_object_or_404(Post, id=post_id)
